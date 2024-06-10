c3b2217ce97446649a7bf340e152275833a4fbfa
Ayush Thakur <ayt12321232@gmail.com>
2024-06-10 23:35:24 +0530

    Added discord url

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -19,7 +19,7 @@ function Footer() {
                     </svg>
                 </Link>
 
-                <Link to='https://discord.com/channels/@_ayush_thakur_' title='Discord' target='_blank' className='hover:opacity-70'>
+                <Link to='https://discordapp.com/users/1224382347655381043' title='Discord' target='_blank' className='hover:opacity-70'>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
                         <path fill="currentColor" d="M17.32 5H6.68a4 4 0 0 0-3.978 3.59c-.006.052-.01.101-.017.152C2.604 9.416 2 14.456 2 16a3 3 0 0 0 3 3c1 0 1.5-.5 2-1l1.414-1.414A2 2 0 0 1 9.828 16h4.344a2 2 0 0 1 1.414.586L17 18c.5.5 1 1 2 1a3 3 0 0 0 3-3c0-1.545-.604-6.584-.685-7.258-.007-.05-.011-.1-.017-.151A4 4 0 0 0 17.32 5z"
                             stroke="none" />
